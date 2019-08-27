1a0448b649b489985fb23de46dfe49dee7297374
Timothy Myers <tim@ip-10-1-10-15.ec2.internal>
2019-08-26 18:03:47 -0600

    kubeconfig file

--- tests/integration/entrypoint.sh
+++ tests/integration/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
+sed -i 's/localhost/server/g' /kubeconfig/kubeconfig.yaml
 export PULUMI_CONFIG_PASSPHRASE=hello
+export KUBECONFIG=/kubeconfig/kubeconfig.yaml
+
 pulumi login --local
 pulumi stack init test || pulumi stack select test
 pulumi up -y --skip-preview
